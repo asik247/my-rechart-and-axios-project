acb19325d6d9680292ca76751681c7f85c895c0e
Asikur Rahman <mdasikur5893@gmail.com>
2025-09-28 23:03:21 +0600

    footer add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import PrichingCard from './components/PrichingCard/PrichingCard';
 import ReChart from './components/ReCharts/ReChart';
+import Footer from './components/Footer/Footer';
 
 const prichingPromise = async ()=>{
   const res = await fetch('/priching.json');
@@ -18,6 +19,7 @@ const App = () => {
        <PrichingCard prichingData={prichingData}></PrichingCard>
     </Suspense>
     <ReChart></ReChart>
+    <Footer></Footer>
     
 
     
